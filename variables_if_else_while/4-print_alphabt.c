72305b3bb8faf9ab562b2fb8cbc604ac8e332b2c
jess6718 <6718@holbertonstudents.com>
2023-06-18 22:09:20 -0700

    first attempt

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Print a-z except 'q' and 'e'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char low = 'a';
+
+	while (low <= 'z')
+	{
+		if (low != 'e' && low !='q')
+		putchar(low);
+		low++;	
+	}
+	putchar('\n');
+	return (0);
+}
