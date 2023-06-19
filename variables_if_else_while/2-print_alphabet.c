747d65d6ae97983278402f038eebf671fa895851
jess6718 <6718@holbertonstudents.com>
2023-06-18 21:23:35 -0700

    first attempt

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Print a-z
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char n = 'a';
+
+	while (n <= 'z')
+		{putchar(n);
+		n++;
+	}
+	return (0);
+}
