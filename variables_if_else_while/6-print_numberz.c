9dfa311294ebbb92a474aa126732978e6f0e135e
jess6718 <6718@holbertonstudents.com>
2023-06-18 23:37:17 -0700

    first attempt

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Print 1-10 without char type
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		putchar(48 + n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
