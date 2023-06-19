8dff9afa7ef5b072c57f2164b5c103880642952e
jess6718 <6718@holbertonstudents.com>
2023-06-19 00:15:51 -0700

    first attempt

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Print all numbers of base 16
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n = 0;
+	char c = 'a';
+
+	while (n < 10)
+	{
+		putchar(48 + n);
+		n = n + 1;
+	}
+	while ( c <= 'f')
+		{putchar(c);
+		c = c + 1;
+	}
+	putchar('\n');
+	return (0);
+}
