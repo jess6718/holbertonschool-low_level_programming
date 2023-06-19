af78f6be1b6980e254cbd2638e0353ff52d38bdf
jess6718 <6718@holbertonstudents.com>
2023-06-18 19:15:36 -0700

    fix zero

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -19,7 +19,7 @@ int main(void)
 		{ printf("%d is positive\n", n);
 	}
 	else if (n == 0)
-		{ printf("%d is is zero\n", n);
+		{ printf("%d is zero\n", n);
 	}
 	else
 		{ printf("%d is negative\n", n);
