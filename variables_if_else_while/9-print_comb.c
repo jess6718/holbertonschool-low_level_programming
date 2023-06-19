b578e1273fb88e076d4d64112db70026185317b6
jess6718 <6718@holbertonstudents.com>
2023-06-19 00:51:33 -0700

    first attempt

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - print combination of single-digit numbers
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n = 0;
+	int comma = 44;
+	int space = 32;
+
+	while (n < 10)
+	{
+		putchar(48 + n);
+		if (n < 9) {
+			putchar(comma);
+			putchar(space);
+		}
+		n = n + 1;
+	}
+	putchar('\n');
+	return (0);
+}
