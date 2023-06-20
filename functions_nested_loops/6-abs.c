485da8180fc2bc6ddce9b7b0c898c99ee467a1f1
jess6718 <6718@holbertonstudents.com>
2023-06-20 04:41:51 -0700

    first attempt

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs - absolute
+ * @n: input int
+ * Description: prints absolute value of the number
+ * Return: absolute value
+ */
+
+int _abs(int n)
+{
+
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
