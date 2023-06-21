3663b10dbd562d8a87d4e1af6c3db8c2678cf74d
jess6718 <6718@holbertonstudents.com>
2023-06-20 19:28:29 -0700

    first attempt

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - add 2 int
+ * @a: int a
+ * @b: int b
+ * Description: add two int
+ * Return: a + b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -10,3 +10,5 @@ int print_last_digit(int);
 void jack_bauer(void);
 int add(int, int);
 void times_table(void);
+int add(int, int);
+
