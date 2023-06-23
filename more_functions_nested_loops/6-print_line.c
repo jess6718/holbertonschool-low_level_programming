85b5e49da47d07df968d1b1ab5e7b739b3ef1ddb
jess6718 <6718@holbertonstudents.com>
2023-06-22 18:49:47 -0700

    first attempt

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_line - draws "___"
+ * @n: input int
+ * Return: 0
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	i = 0;
+	if (n > 0)
+	{
+		while (i < n)
+		{
+			_putchar('_');
+			i = i + 1;
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
