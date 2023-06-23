3ad1906298985119c333c06679aaa18f14926f24
jess6718 <6718@holbertonstudents.com>
2023-06-22 22:56:02 -0700

    update

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -18,7 +18,7 @@ int main(void)
 		else if (n % 5 == 0)
 			printf("Buzz ");
 		else if (n % 3 == 0 && n % 5 == 0)
-			printf("FizzBuzz");
+			printf("FizzBuzz ");
 		else
 			printf("%d ", n);
 		n = n + 1;
