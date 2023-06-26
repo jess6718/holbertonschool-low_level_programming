6c0d27fd885bf0251fc1dad4054be447c12ab8ec
jess6718 <6718@holbertonstudents.com>
2023-06-26 05:22:56 -0700

    init

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse a string
+ * @s: a string
+ * Return: Nothing
+ */
+void rev_string(char *s)
+{
+	int len;
+	int i;
+	char tmp;
+
+	len = 0;
+	i = 0;
+
+	while (s[len] != '\0')
+	{
+
+		len = len + 1;
+	}
+
+	while (i < len--)
+	{
+		tmp = s[i];
+		s[i++] = s[len];
+		s[len] = tmp;
+	}
+}
