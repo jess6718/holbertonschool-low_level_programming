0fd5ba5886f8149ab90138aaf4e0a39638a6bb8a
jess6718 <6718@holbertonstudents.com>
2023-06-27 16:14:30 -0700

    first

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: a string
+ * @s2: a string
+ * Return: The difference of s1 and s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int len_s1;
+	int len_s2;
+
+	len_s1 = 0;
+	len_s2 = 0;
+	while (len_s1 != '\0' && len_s2 != '\0')
+	{
+		/* calculate difference of the first non - matching character */
+		if (s1[len_s1] != s2[len_s2])
+		{
+			return (s1[len_s1] - s2[len_s2]);
+			len_s1 = len_s1 + 1;
+                	len_s2 = len_s2 + 1;
+		}
+	}
+	return (s1[len_s1] - s2[len_s2]);
+}
