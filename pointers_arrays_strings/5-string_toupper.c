09475450ce5eb7ff9f9a429d6e52d9daef5b5ebb
jess6718 <6718@holbertonstudents.com>
2023-06-27 21:56:22 -0700

    fist

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes lowercase letters of a  string to uppercase
+ * @s: a string
+ * Return: char pointer to converted string
+ */
+char *string_toupper(char *s)
+{
+	char *start;
+	int index;
+
+	index = 0;
+	start = s;
+	while (s[index] != '\0')
+	{
+		if (s[index] > 'a' && s[index] < 'z')
+			s[index]  = s[index] - ('a' - 'A');
+		index = index + 1;
+	}
+	return (start);
+}
