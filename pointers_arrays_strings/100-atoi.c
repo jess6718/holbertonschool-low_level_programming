455721ad343391515eabbaa1ac6a503f47b037ef
jess6718 <6718@holbertonstudents.com>
2023-06-28 04:11:54 -0700

    update

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -9,31 +9,26 @@ int _atoi(char *s)
 {
 	int i;
 	int sign;
-	int result;
-	char no_number;
+	unsigned int tmp_number;
 
 	i = 0;
 	sign = 1;
-	result = 0;
-	no_number = 0;
-	while (s[i] != '\0')
+	tmp_number = 0;
+	while (s[i] != '\0' && (s[i] < '0' || s[i] > '9'))
 	{
 		if (s[i] == '-')
-		sign = sign * -1;
-		/* convert string to number */
-		if (s[i] >= '0' && s[i] <= '9')
 		{
-			no_number = 1;
-			result = result * 10 + s[i] - '0';
+			sign = sign * -1;
 		}
-		else if (no_number == 1)
-		break;
-
 		i = i + 1;
 	}
-
-	if (sign < 0)
-	result = (-result);
-
-	return (result);
+	if (s[i] != '\0')
+	{
+		while (s[i] >= '0' && s[i] <= '9')
+		{
+			tmp_number = tmp_number * 10 + (s[i] - '0');
+			i = i + 1;
+		}
+	}
+	return (tmp_number * sign);
 }
