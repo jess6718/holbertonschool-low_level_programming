4c38f6159f29d3fca25d40bf720743908fa63c0b
jess6718 <6718@holbertonstudents.com>
2023-06-28 18:42:51 -0700

    first

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memset - fills memory with constant bytes
+ * @s: location to fill
+ * @b: char to fill memory with
+ * @n: size of memory block to fill
+ * Return: returns pointer to memory filled
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	i = 0;
+	while (i < n)
+	{
+		s[i] = b;
+		i = i + 1;
+	}
+	return (s);
+}
