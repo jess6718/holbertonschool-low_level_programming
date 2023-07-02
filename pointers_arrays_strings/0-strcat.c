0b5eea034008c0d33adce41b424cd06771b967b2
jess6718 <6718@holbertonstudents.com>
2023-07-01 22:26:19 -0700

    refactor

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -8,18 +8,18 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	char *tmp;
 	int len_d;
 	int len_s;
 
 	len_d = 0;
 	len_s = 0;
-	tmp = dest;
 	while (dest[len_d] != '\0')
 	{
 		len_d = len_d + 1;
 	}
-	/* append src to dest */
+	/** append src to dest, no need to len_d = len_d - 1
+	as overwrite '\0' 
+	 */
 	while (src[len_s] != '\0')
 	{
 		dest[len_d] = src[len_s];
@@ -27,5 +27,5 @@ char *_strcat(char *dest, char *src)
 		len_d = len_d + 1;
 	}
 	dest[len_d] = '\0';
-	return (tmp);
+	return (dest);
 }
