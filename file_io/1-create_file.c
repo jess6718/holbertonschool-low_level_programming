6035cccc72874836fe07999ead59c103c3a7ab49
jess6718 <6718@holbertonstudents.com>
2023-08-06 23:40:16 -0700

    fix text_content is NULL

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -37,11 +37,14 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	/* call _strlen to get number of text_content passed in*/
-	num_written = write(fd, text_content, _strlen(text_content));
-	if (num_written == -1) /*if write fails*/
+	if (text_content != NULL)
 	{
-		return (-1);
+		/* call _strlen to get number of text_content passed in*/
+		num_written = write(fd, text_content, _strlen(text_content));
+		if (num_written == -1) /*if write fails*/
+			{
+				return (-1);
+			}
 	}
 	close(fd);
 	return (1);
